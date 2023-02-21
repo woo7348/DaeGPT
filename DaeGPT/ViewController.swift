8a4905d4a7717c318eb1142f0b95fdcebe6470b0
MinWoo <mynooo7688@gmail.com>
2023-02-21 23:26:46 +0900

    Initial Commit

---
+++ DaeGPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaeGPT
+//
+//  Created by 강민우 on 2023/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
